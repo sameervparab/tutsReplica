f6acdf9350c65b1673f9f2d717746fe9acb6ea2d
Sameer <samparab79@gmail.com>
2020-12-09 16:28:51 +0530

    init setup

---
+++ src/components/Book/Book.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import './index.scss';
+
+const Book = (props) => {
+    const { imgSrc, title, author, amount, children } = props;
+    const pressEvent = () => { 
+        alert(title)
+    }
+    return (
+			<div class='book'>
+				<img src={imgSrc} alt='' />
+				<h2>{title}</h2>
+				<div class='author'>By {author} </div>
+				<p>Rs. {amount}</p>
+				<p>{children}</p>
+				<Button
+					onClick={pressEvent}
+					variant='primary'
+				>
+					Primary
+				</Button>
+			</div>
+		);
+}
+
+export default Book
---
+++ src/components/BookList/BookList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Book from '../Book/Book'
+import {data} from './books'
+import './index.css'
+import { v4 as uuidv4 } from 'uuid';
+
+const BookList = () => {
+    return (
+        <div className="book-list">
+            {data.map((book) => { 
+            return <Book {...book}/>
+            })}
+        </div>
+    )
+}
+
+export default BookList
